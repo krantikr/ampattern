be169cdf71064b1435d0fed6ff38a295f0d9c0da
kranti kr <kranti_kumar@hotmail.com>
2016-03-06 21:43:37 +0530

    added dynamic question size

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,18 @@
 module HomeHelper
+	def aptitude_size
+		Question.where(question_type_id: 1).size
+	end
+
+
+	def computer_size
+		Question.where(question_type_id: 2).size
+	end
+
+	def english_size
+		Question.where(question_type_id: 3).size
+	end
+
+	def reasoning_size
+		Question.where(question_type_id: 4).size
+	end
 end
