cd4c85c0d7c3b82980c6a8b8518c10abe9aeee71
kranti kr <kranti_kumar@hotmail.com>
2016-03-06 23:29:58 +0530

    add question is added in index page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
 
-def company_name(id)
-	Company.find(id).name
-end
+	def company_name(id)
+		Company.find(id).name
+	end
 
-def question_name(id)
-	QuestionType.find(id).question_type
-end
+	def question_name(id)
+		QuestionType.find(id).question_type
+	end
 
 end
