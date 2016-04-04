7bab6424ce893e19e1b7769cf686cad6ddd9fc6d
kranti kr <kranti_kumar@hotmail.com>
2016-04-05 00:12:03 +0530

    pagination view and adds added

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -1,7 +1,8 @@
 class CompanyController < ApplicationController
 
   def company
-    @questions = Company.find_by(name: params[:company_name]).question.where(approve: true)
+    questions = Company.find_by(name: params[:company_name]).question.where(approve: true)
+    @questions = questions.order("created_at DESC").page params[:page]
   end
 
 end
